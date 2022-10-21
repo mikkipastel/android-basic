fba26f3b9e22f8f01c1a40660f3717e88e2dc49d
Monthira Chayabanjonglerd <monthira.cha@ascendcorp.com>
2022-10-21 14:49:30 +0700

    [2] typo name for first_name and last_name

--- app/src/main/java/com/ascedncorp/androidbasic/model/UserListResponse.kt
+++ app/src/main/java/com/ascedncorp/androidbasic/model/UserListResponse.kt
@@ -14,8 +14,8 @@ data class UserListResponse(
 data class DataItem(
     @SerializedName("id") val id: Int? = null,
     @SerializedName("email") val email: String? = null,
-    @SerializedName("first_name") val first_name: String? = null,
-    @SerializedName("last_name") val last_name: String? = null,
+    @SerializedName("first_name") val firstName: String? = null,
+    @SerializedName("last_name") val lastName: String? = null,
     @SerializedName("avatar") val avatar: String? = null,
 )
 
